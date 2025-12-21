0fa462390e5c91d919739740e8cbae283e570cd4
Sam Lando <sam.bell.lando@gmail.com>
2025-12-20 21:43:40 -0800

    update cowlib dep version to work with elixir 1.19

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule SlPortfolio.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.7"}
     ]
   end
 
